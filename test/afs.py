e9945c91ed6f5347998a5722266a3dbed3fc6299
Elizabeth Ditton (Forest) <elizabeth.forest@my.jcu.edu.au>
2022-05-26 14:51:08 +1000

    fix requirements.txt

--- test/afs.py
+++ test/afs.py
@@ -4,7 +4,7 @@ import unittest
 class AFSTest(unittest.TestCase):
 
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(True, True)  # add assertion here
 
 
 if __name__ == '__main__':
